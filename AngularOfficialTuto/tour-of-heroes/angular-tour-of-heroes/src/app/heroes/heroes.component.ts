4da650c34361992bc30daa62e8e241520324eb52
nantunes <nicolas.antunes@ubicast.eu>
2021-10-07 18:16:27 +0200

    add heros, hero-detail, message component and services; init routing

--- AngularOfficialTuto/tour-of-heroes/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ AngularOfficialTuto/tour-of-heroes/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,19 +1,35 @@
+import { Message } from '@angular/compiler/src/i18n/i18n_ast';
 import { Component, OnInit } from '@angular/core';
-import { Hero } from '../hero';
 
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+  heroes : Hero[] = [];
+  selectedHero?: Hero;
 
-  constructor() { }
+  constructor(
+    private heroService: HeroService,
+    private messageService: MessageService
+    ) { }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes)
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: selected hero id = ${hero.id}`)
+  }
+
 
   ngOnInit(): void {
+    this.getHeroes();
   }
 }
